9c8f9098cab7681e1528791775e4e61c7809d763
data8320-24 <sksksmssksml@gmail.com>
2026-04-10 08:56:36 +0900

    Scanner 사용 흐름 복습

--- src/main/java/Code02_10.java
+++ src/main/java/Code02_10.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class Code02_10 {
     public static void main(String[] args) {
+
         Scanner s = new Scanner(System.in);
 
         int num;
 
         num = s.nextInt();
-        System.out.println("사용자가 입력한 값 ==>" + num);
+        System.out.println("숫자를 입력해 주세요==>" +num);
 
         s.close();
     }
