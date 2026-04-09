83035acddc22b4bba10903935f70c2b217fc2854
data8320-24 <sksksmssksml@gmail.com>
2026-04-10 00:09:05 +0900

    문자열연산 복습 암기

---
+++ src/main/java/Code02_008.java
@@ -0,0 +1,13 @@
+public class Code02_008 {
+    public static void main(String[] args) {
+
+        String str1 = "난생처음";
+        String str2 = "자바";
+
+        System.out.print("난생처음");
+        System.out.println("자바");
+
+        String result = (str1+str2);
+        System.out.println(result);
+    }
+}
